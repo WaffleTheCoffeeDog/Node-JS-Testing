8dfe6845652ac124191b066b08bf7f36b188fa44
WaffleTheCoffeeDog <786253@dpsk12.net>
2025-02-13 06:01:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+io.on('connection', (socket) => {
+   console.log('A user connected');
+   socket.on('disconnect', () => {
+      console.log('A user disconnected');
+   });
+
+   socket.on('sendAlert', () => {
+      io.emit('receiveAlert', 'This is a universal alert!');
+   });
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
