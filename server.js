a4349c111e8732d3e74bb388034d887598d62a18
WaffleTheCoffeeDog <786253@dpsk12.net>
2025-02-22 13:52:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,33 @@
-// ?????
 const helmet = require("helmet");
 const mongoose = require("mongoose");
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-module.exports = { io };
 const fs = require("fs");
 const cookieParser = require("cookie-parser");
 const rateLimit = require("express-rate-limit");
 
-//  :)
-
-const express = require('express');
-const app = express();
 const path = require('path');
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-   console.log('A user connected');
-   socket.on('disconnect', () => {
-      console.log('A user disconnected');
-   });
-
-   socket.on('sendAlert', () => {
-      io.emit('receiveAlert', 'This is a universal alert!');
-   });
+    console.log('a user connected');
+    socket.on('sendAlert', () => {
+        socket.emit('receiveAlert', 'Alert received!');
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+server.listen(3000, () => {
+    console.log('listening on *:3000');
 });
